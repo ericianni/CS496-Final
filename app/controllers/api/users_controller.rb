9e614ae1686d2965ec0f56f18672228581db8f6e
ericianni <ebianni@gmail.com>
2015-08-13 00:46:09 -0400

    changed json to return id number

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,7 +6,7 @@ class Api::UsersController < ApplicationController
   def login
     user = User.find_by(email: user_params[:email])
     if user && user.authenticate(user_params[:password])
-      render json: '{"status":"success"}'
+      render json: '{"status":"success", "id":"#{user.id}"}'
     else
       render json: '{"status":"failure"}'
     end
@@ -15,7 +15,7 @@ class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: '{"status":"success"}'
+      render json: '{"status":"success", "id":"#{@user.id}"}'
     else
       render json: '{"status":"failure"}'
     end
