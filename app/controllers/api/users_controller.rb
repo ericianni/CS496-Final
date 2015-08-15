243b67b76079abb6bee1d36109913640b357ebc6
ericianni <ebianni@gmail.com>
2015-08-14 20:31:39 -0400

    I needed a PUT so update User

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -35,6 +35,14 @@ class Api::UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      render json: '{"status":"success"}'
+    else
+      render json: '{"status":"failure"}'
+  end
+  
   def show
     @user = User.find(params[:id])
   end
