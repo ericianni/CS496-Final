cc5e7a36e3081b1f464d1af75865c2711b72774e
ericianni <ebianni@gmail.com>
2015-08-14 21:19:52 -0400

    crap

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -40,7 +40,8 @@ class Api::UsersController < ApplicationController
     if @user.update_attributes(user_params)
       render json: '{"status":"success"}'
     else
-      render json: '{"status":"failure"}'
+      render json: user_params
+#      render json: '{"status":"failure"}'
     end
   end
   
